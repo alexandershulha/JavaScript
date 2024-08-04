d67459a6b8f64f9c3cd0036e14f79bbf3e363463
Alexandr Shulha <shulga197100@gmail.com>
2024-08-04 14:04:25 +0300

    Changes

---
+++ Lesson2/task1/index.js
@@ -0,0 +1,10 @@
+const userName = 'Jone';
+console.log(userName);
+const greeting1 = " I'm Jone ";
+console.log(greeting1);
+const hobby = 'Walking';
+console.log(hobby);
+const favouriteDessert = 'cake';
+console.log(favouriteDessert);
+const superNumber = 17
+console.log(superNumber);
